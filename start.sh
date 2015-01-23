668f3c4223067815cd015d2222b6161cc81ae079
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-24 00:48:13 +1300

    Now managing history

--- start.sh
+++ start.sh
@@ -7,3 +7,11 @@ echo "\033[0;32m" '| |\ \ | |_| || |_) ||  __/| |_\ \| (_) || || (_| |  / /_/ / 
 echo "\033[0;32m" '\_| \_| \____||____/  \___| \____/ \___/ |_| \____/ /_____/\____/____/_/ /_/ ' "\033[0m"
 echo "\033[0;32m"'....is now installed!'"\033[0m"
 echo "\n\n \033[0;32mp.s. Follow us at http://twitter.com/TODO.\033[0m"
+
+# Configure history to work the way we want to.
+HISTFILE=~/.rubegoldbash_history
+HISTIGNORE=ls:'cd:cd -:pwd:exit:date:* --help:ls:'
+HISTTIMEFORMAT='%F %T '
+HISTSIZE=10000
+SAVEHIST=10000
+HISTCONTROL=ignorespace:ignoredups:erasedups
